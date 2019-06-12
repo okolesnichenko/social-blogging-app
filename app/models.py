3cf9fa85aefbb4c8349aa1120eda5d16c34f3da1
worldintro <worldintro>
2019-06-12 14:14:32 +0300

    v0.1

--- app/models.py
+++ app/models.py
@@ -196,7 +196,6 @@ class Follow(db.Model):
 
 
 class Role(db.Model):
-
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
