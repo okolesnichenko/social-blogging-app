8a8fe0c7b367096d12ffaa084a9b8902e0c026e2
worldintro <worldintro>
2019-06-12 12:57:18 +0300

    v0.1

--- manage.py
+++ manage.py
@@ -50,8 +50,6 @@ def deploy():
     from flask_migrate import upgrade
     from app.models import Role, User
 
-    # migrate database to latest revision
-    upgrade()
 
     # create user roles
     Role.insert_roles()
